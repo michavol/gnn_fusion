0a577dfa841924b505e4bd1ecf1c534b0f977726
ehoskovec <ehoskovec@student.ethz.ch>
2023-12-03 19:02:21 +0100

    add: ensembling of networks

--- gnn_benchmarking/main_molecules_graph_regression.py
+++ gnn_benchmarking/main_molecules_graph_regression.py
@@ -1,8 +1,4 @@
 
-
-
-
-
 """
     IMPORTING LIBS
 """
--- src/baseline/ensemble.py
+++ src/baseline/ensemble.py
@@ -3,7 +3,24 @@
 import argparse
 from typing import List
 
+import torch
+import torch.nn as nn
+
 from torch.utils.data import DataLoader
+
+class Ensemble(nn.Module):
+    def __init__(self, models):
+        super(Ensemble, self).__init__()
+        self.models = nn.ModuleList(models)
+
+    def forward(self, g, h, e):
+        outputs = [model.forward(g, h, e) for model in self.models]
+        print(outputs)
+        return torch.mean(torch.stack(outputs), dim=0)
+    
+    def loss(self,scores, targets):
+        outputs = [model.loss(scores, targets) for model in self.models]
+        return torch.mean(torch.stack(outputs), dim=0)
 
 def compose_models(args: argparse.Namespace, models: List, test_loader: DataLoader) -> float:
     pass
